3b81b4fc70d6c847b3ad9eb716d832ad8c7c13a1
yvonneanne <yvonneanne@dfinity.org>
2023-02-07 13:11:41 +0100

    pair

--- frontend/src/crypto.test.js
+++ frontend/src/crypto.test.js
@@ -3,7 +3,7 @@ const {generateKeypair, encrypt, decrypt } = require('./crypto');
 test('generate key, encrypt and decrypt', async () => {
   plaintext = "blabla42";
 
-  const key = await generateKey();
+  const key = await generateKeypair();
   const {
     privateKey,
     publicKey
@@ -12,7 +12,7 @@ test('generate key, encrypt and decrypt', async () => {
   
   const encryptedText = await encrypt(plaintext, publicKey);
 
-  const decryptedText = await decrypt(encryptedText, privateKeypair);
+  const decryptedText = await decrypt(encryptedText, privateKey);
 
   expect(plaintext).toBe(decryptedText);
 });
