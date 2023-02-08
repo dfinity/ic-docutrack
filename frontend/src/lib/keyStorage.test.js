1331d94dd4b073429bef97abafc98465d44c8d9b
yvonneanne <yvonneanne@dfinity.org>
2023-02-08 14:22:59 +0100

    add tests

--- frontend/src/lib/keyStorage.test.js
+++ frontend/src/lib/keyStorage.test.js
@@ -1,17 +1,26 @@
 require("fake-indexeddb/auto");
-const { clearKeys } = require('./keyStorage');
+const { clearKeys, storeKey, loadKey } = require('./keyStorage');
 const { generateUserKeypair } = require('./crypto');
 
 test('create, store, load and clear keys', async () => {
 
+  // loading a key which has not been stored yet fails
+  const load_empty = await loadKey('private');
+  expect(load_empty).toBeFalsy();
+  
+  // loading a key which has been stored succeeds
   const key = await generateUserKeypair();
   const {
     privateKey,
     publicKey
   } = key;
-  
-  
-  const load_empty = await clearKeys();
+  storeKey('private', privateKey);
+  const load_private = await loadKey('private');
+  expect(load_private).toStrictEqual(privateKey);
 
-  console.log( load_empty);
+  // loading a key after clearing fails
+  clearKeys();
+  const load_empty2 = await loadKey('private');
+  expect(load_empty2).toBeFalsy();
+  
 });
