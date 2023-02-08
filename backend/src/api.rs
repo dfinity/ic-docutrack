c3729cc2bf1240a475765ebff49df96bc485567d
Islam El-Ashi <islam.elashi@dfinity.org>
2023-02-08 10:59:09 +0100

    feat: add upload_file_atomic API

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,5 +1,7 @@
 mod request_file;
 mod upload_file;
+mod upload_file_atomic;
 
 pub use request_file::request_file;
 pub use upload_file::upload_file;
+pub use upload_file_atomic::{upload_file_atomic, UploadFileAtomicRequest};
