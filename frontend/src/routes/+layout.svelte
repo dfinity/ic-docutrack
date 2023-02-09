d7d467a814db05672938804bd3386c9b438d97ab
Michel Perez <michel.perez@gmx.ch>
2023-02-09 06:48:19 +0100

    add container wrapper and delete boilerplate code

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,51 +1,12 @@
 <script>
+	import {Container} from 'sveltestrap';
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
 <div class="app">
 	<Navbar />
 
-	<main>
+	<Container class="pt-4">
 		<slot />
-	</main>
-
-	<footer>
-	</footer>
+	</Container>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
