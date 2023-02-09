0d6d13578fb3c2f39fb809c65ae4b046a8451b0a
Michel Perez <michel.perez@gmx.ch>
2023-02-10 00:28:05 +0100

    integrate stores with II auth

--- frontend_new/src/frontend/src/lib/shared/stores/auth.js
+++ frontend_new/src/frontend/src/lib/shared/stores/auth.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const principal = writable("");
+export const principal = writable();
 export const firstName = writable("");
 export const lastName = writable("");
 export const actor = writable();
