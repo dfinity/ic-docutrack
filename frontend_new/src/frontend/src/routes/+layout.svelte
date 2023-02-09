91d3d8385f70766b19bf3c5c5d10ada0ac6e2467
Nelson Saturno <nelsonsaturno@gmail.com>
2023-02-09 19:00:56 +0100

    migrating frontend

---
+++ frontend_new/src/frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {Container} from 'sveltestrap';
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<div class="app">
+	<Navbar />
+	<Container class="pt-4">
+		<slot />
+	</Container>
+</div>
