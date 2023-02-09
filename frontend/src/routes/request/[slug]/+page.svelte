30813e3b98c84bd577518b481c4f92c06c770b36
Michel Perez <michel.perez@gmx.ch>
2023-02-09 11:10:55 +0100

    add preview

--- frontend/src/routes/request/[slug]/+page.svelte
+++ frontend/src/routes/request/[slug]/+page.svelte
@@ -1,8 +1,12 @@
 <script>
 	import { Table } from 'sveltestrap';
+	import FilePreview from '../../../lib/components/FilePreview.svelte';
 
 	export let data;
 	let isFileSubmitted = false;
+	let files;
+	let file;
+	let fileData;
 
 	function sendFile() {
     	// TODO: Add file encryption and submission to backend
@@ -10,6 +14,37 @@
 		// When successful, set flag
 		isFileSubmitted = true;
 	}
+
+	function onChange() {
+	 if(files) {
+		let inputFile = files[0];
+		let dataBase64;
+		
+		const reader = new FileReader();
+		
+		// reader.onload = function () {
+		// 	// dataBase64 = reader.result;
+		// 	log.console(reader.result)
+		// };
+		// reader.onerror = function (error) {
+		// 	console.log('Error: ', error);
+		// };
+		file = {
+			name: inputFile.name,
+			dataType: inputFile.type,
+			data: '',
+		};
+
+		reader.readAsDataURL(inputFile);
+		reader.onload = function () {
+			let base64 = reader.result;
+			let pattern = 'base64,';
+			let idx = base64.indexOf('base64,');
+			file.data = base64.substring(idx+pattern.length);
+		};
+		return;
+	}
+}
 </script>
 
 <svelte:head>
@@ -25,9 +60,16 @@
 	{:else}
 		<div class="my-3">
 			<label for="formFile" class="form-label">Upload your file</label>
-			<input class="form-control" type="file" id="formFile">
+			<input bind:files on:change={onChange} class="form-control" type="file" id="formFile">
 		</div>
-		<button on:click={sendFile} class="btn btn-primary">Submit</button>
+		<div class="mb-3">
+			<button on:click={sendFile} class="btn btn-primary">Submit</button>
+		</div>
+		
+		{#if file && file.data}
+		<h4>File Preview</h4>
+			<FilePreview {file}/>
+		{/if}
 	{/if}
 	
 </section>
