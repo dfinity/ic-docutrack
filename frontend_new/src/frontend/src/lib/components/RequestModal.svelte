5e329084eb6f25cdad7f4cd694b01b180aebc484
Nelson Saturno <nelsonsaturno@gmail.com>
2023-02-13 15:01:32 +0100

    Request File functionality

--- frontend_new/src/frontend/src/lib/components/RequestModal.svelte
+++ frontend_new/src/frontend/src/lib/components/RequestModal.svelte
@@ -1,63 +1,92 @@
-<script>
+<script lang="ts">
 	import {
 		Button,
 		Modal,
 		ModalBody,
-		ModalFooter,
-		ModalHeader,
-		Form,
-		FormGroup,
-		Input,
-		Label
+		ModalHeader
 	} from 'sveltestrap';
+	import { page } from '$app/stores';
 
 	export let isOpen = false;
 	const toggle = () => (isOpen = !isOpen);
 
 	import { actor } from '$lib/shared/stores/auth.js';
+	import { createActor } from '../../../../declarations/backend';
 
-	let actorValue;
-	let requestName;
-	let requestUrl;
+	let actorValue: object;
+	let requestName: string;
+	let requestLink: URL;
+	let loading: boolean = false;
 
 	actor.subscribe((value) => (actorValue = value));
 
+	// Canister IDs are automatically expanded to .env config - see vite.config.ts
+	const canisterId = import.meta.env.VITE_BACKEND_CANISTER_ID;
+	// We pass the host instead of using a proxy to support NodeJS >= v17 (ViteJS issue: https://github.com/vitejs/vite/issues/4794)
+	const host = import.meta.env.VITE_HOST;
+	// Create an actor to interact with the IC for a particular canister ID
+	actor.set(createActor(canisterId, { agentOptions: { host } }));
+
 	async function updateRequestUrl(e) {
+		loading = true;
 		const formData = new FormData(e.target);
-		// Do not request new url when there is already one
-		const data = {};
+		const data: any = {};
 		for (let field of formData) {
 			const [key, value] = field;
 			data[key] = value;
 		}
-		if (data.requestName || !data.requestLink) return;
-		requestUrl = await actor.request_file(data.request_name);
-		// Update request view
+
+		// Do not request new url when there is already one
+		if (data.requestName && !data.requestLink) {
+			requestName = data.requestName;
+			const alias = await actorValue.request_file(data.requestName);
+			requestLink = new URL($page.url.origin + "/upload");
+			requestLink.searchParams.append("alias", alias);
+		}
+		loading = false;
 	}
+
+	function copyText() {
+		var copyText = "";
+		if (requestLink) {
+			copyText = requestLink;
+		}
+		navigator.clipboard.writeText(copyText);
+		// Alert the copied text
+		alert("Copied!");
+	}
+
 </script>
 
 <div>
 	<Button color="default" on:click={toggle}>Create new request</Button>
 	<Modal {isOpen} {toggle}>
 		<ModalHeader {toggle}>Create Request</ModalHeader>
-		<Form>
-			<ModalBody>
-				<div class="mb-3">
-					<label for="requestName" class="form-label">Request Name</label>
-					<input type="text" class="form-control" id="requestName" bind:value={requestName} />
+		<ModalBody>
+			<form class="form-floating" on:submit|preventDefault={updateRequestUrl}>
+				<div class="form-floating mb-3">
+					<input type="text" required={true} class="form-control" id="requestName" placeholder="Enter your input" name="requestName">
+					<label for="requestName">Request Name</label>
 				</div>
-				<div class="mb-3">
-					<label for="requestLink" class="form-label">Request Link</label>
-					<input class="form-control" id="requestLink" bind:value={requestUrl} readonly />
+				<div class="form-floating mb-3">
+					{#if requestLink}
+						<input type="text" class="form-control" id="requestLink" placeholder="" name="requestLink" value={requestLink} readonly>
+					{:else}
+						<input type="text" class="form-control" id="requestLink" placeholder="" name="requestLink" readonly>
+					{/if}
+					<label for="requestLink">Request Link</label>
 				</div>
-			</ModalBody>
-			<ModalFooter>
-				{#if requestUrl}
-					<Button color="primary" on:click={toggle}>Close</Button>
-				{:else}
-					<Button color="primary" type="submit" on:click={updateRequestUrl}>Create Request</Button>
-				{/if}
-			</ModalFooter>
-		</Form>
+				<div class="form-floating mb-3">
+					<button class="btn btn-outline-secondary" type="button" on:click={copyText}>Copy Link</button>
+					{#if loading}
+						<button type="submit" class="btn btn-primary" disabled>Loading</button>
+					{:else if !loading && requestLink}
+						<button type="submit" class="btn btn-primary" disabled>Submit</button>
+					{:else}
+						<button type="submit" class="btn btn-primary">Submit</button>
+					{/if}
+				</div>
+			</form>
+		</ModalBody>
 	</Modal>
 </div>
