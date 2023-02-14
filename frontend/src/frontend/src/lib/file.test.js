195475a8468d5d8c74de5c6ecbfd915ca568d491
Nelson Saturno <nelsonsaturno@gmail.com>
2023-02-14 10:53:08 +0100

    Fix test

--- frontend/src/frontend/src/lib/file.test.js
+++ frontend/src/frontend/src/lib/file.test.js
@@ -1,20 +1,20 @@
-// import 'fake-indexeddb/auto';
-// import default from './crypto';
-// const { getLocalUserPublicKey } = default;
-// import File from './file';
+import 'fake-indexeddb/auto';
+import {expect, test} from '@jest/globals';
+import { default as crypto } from './crypto';
+import File from './file';
 
-// test('can encrypt and decrypt a file.', async () => {
-// 	const fileContents = Uint8Array.from([1, 2, 3]).buffer;
-// 	const file = File.fromUnencrypted('file name', fileContents);
+test('can encrypt and decrypt a file.', async () => {
+	const fileContents = Uint8Array.from([1, 2, 3]).buffer;
+	const file = File.fromUnencrypted('file name', fileContents);
 
-// 	const userKey = await getLocalUserPublicKey();
+	const userKey = await crypto.getLocalUserPublicKey();
 
-// 	const encryptedContents = await file.encrypt();
+	const encryptedContents = await file.encrypt();
 
-// 	const encryptedFileKey = await file.getEncryptedFileKey(userKey);
+	const encryptedFileKey = await file.getEncryptedFileKey(userKey);
 
-// 	const file2 = await File.fromEncrypted('file name', encryptedContents, encryptedFileKey);
+	const file2 = await File.fromEncrypted('file name', encryptedContents, encryptedFileKey);
 
-// 	expect(file.contents).toEqual(fileContents);
-// 	expect(file2.contents).toEqual(fileContents);
-// });
+	expect(file.contents).toEqual(fileContents);
+	expect(file2.contents).toEqual(fileContents);
+});
