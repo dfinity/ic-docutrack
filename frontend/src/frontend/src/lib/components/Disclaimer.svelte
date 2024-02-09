761569eb6db03b10bda8532efc143bd9d1e66055
olaszakos <olaszakos@gmail.com>
2024-02-09 14:51:12 +0100

    feat: add disclaimer

---
+++ frontend/src/frontend/src/lib/components/Disclaimer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import WarningIcon from "./icons/WarningIcon.svelte";
+</script>
+
+<div class="max-w-5xl px-4 mx-auto pt-6 text-center">
+  <a
+    href="https://github.com/dfinity/ic-docutrack?tab=readme-ov-file#disclaimer-please-read-carefully"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="inline-flex gap-1 items-center text-text-200 text-body-2 md:text-body-1"
+  >
+    <span class="text-silver-700">
+      <WarningIcon />
+    </span> This is an alpha version. Use at your own risk!
+  </a>
+</div>
