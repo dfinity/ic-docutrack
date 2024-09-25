6256c5c0eb5afc4518fe31d832f70cfc512e0b2c
raymondk <raymond.khalife@dfinity.org>
2024-09-25 16:15:48 -0400

    chore: add required files for custom domains on the marketing page (#18)

--- landing-page/astro.config.ts
+++ landing-page/astro.config.ts
@@ -4,6 +4,8 @@ import { host, readCanisterIds } from "../frontend/scripts/readCanisterIds";
 const canisterIds = readCanisterIds({});
 const hostUrl = new URL(host);
 
+// TODO after the custom domains are registered, the PUBLIC_DAPP_URL
+//      will need to be be set to the custom domain
 if (hostUrl.port) {
   process.env.PUBLIC_DAPP_URL = `${hostUrl.protocol}//${canisterIds.FRONTEND_CANISTER_ID}.${hostUrl.hostname}:${hostUrl.port}`;
 } else {
